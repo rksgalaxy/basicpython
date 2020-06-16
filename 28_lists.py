d384641ecba41c65691f16985869a52adec92434
rksgalaxy <rishikeshkumar4444@gmail.com>
2020-06-17 03:44:31 +0530

    Good Morning again added

--- 28_lists.py
+++ 28_lists.py
@@ -1,12 +1,13 @@
 #Normal Lists
 rks = ["Good","Morning","ALL"]
 print( rks[1],rks[2])
-
 #Empty Lists
 r = []
 print(r)
 print ("RKSGALAXY")
 
 print('GOOD ' + 'NIGHT')
+print('Good Morning again')
+
 
 
