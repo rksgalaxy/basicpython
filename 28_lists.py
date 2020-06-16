84ab89dc651952c972b6cbf9fbbdaa4d5c3a8e07
rksgalaxy <rishikeshkumar4444@gmail.com>
2020-06-17 04:03:35 +0530

    list completed

--- 28_lists.py
+++ 28_lists.py
@@ -3,11 +3,11 @@ rks = ["Good","Morning","ALL"]
 print( rks[1],rks[2])
 #Empty Lists
 r = []
-print(r)
-print ("RKSGALAXY")
-
-print('GOOD ' + 'NIGHT')
-print('Good Morning again')
-
-
-
+#NEsted List
+cc = ['cd']
+s = ['ab', rks]
+print(s[0] , rks[0],rks[1])
+print(s[0],s[1][0])
+#letter in string
+hh = 'Hero Honda'
+print(hh[6])
