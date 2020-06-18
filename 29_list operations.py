0e9c7727cc6e38507879218990d5c1901d54d184
rksgalaxy <rishikeshkumar4444@gmail.com>
2020-06-19 00:59:47 +0530

    list operations and functions

---
+++ 29_list operations.py
@@ -0,0 +1,29 @@
+# reassigning index
+
+nums = [8, 8, 8, 8, 8]
+nums[1] = 10
+print(nums)
+
+# addition and multiplication
+
+nu = [25, 24]
+print(nu + [23, 22, 21])
+print(nu * 2)
+
+#in operators returns true or false
+Names = ["Rishi", "Lucky", "Sundaram"]
+print('Rishi' in Names)
+print("kumar" in Names)
+print("Lucky" in Names)
+print("Sundaram" in Names)
+
+letters = ['a','b','c']
+if "b" in letters:
+    print("Yahoo")
+
+# not operators
+No = [7, 8, 9, 10]
+print(1 not in No)
+print(not 1 in No)
+print(8 not in No)
+print(not 8 in No)
